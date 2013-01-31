9fde23c9ac16617e7e633a90c57c67dfd3516c40
achakravarty <achakravarty@upcurve.in>
2013-01-31 21:49:20 +0530

    Modified the error handling

--- Templatize.js
+++ Templatize.js
@@ -1,18 +1,16 @@
 ﻿(function ($) {
-    $.fn.templatize = function (templateName, model, options) {
+    $.fn.render = function (templateName, model, options) {
         var templatePath = "";
         var templateExtn = ".html";
         var $this = '';
         var settings = $.extend({
             templatePath: templatePath,
             templateType: templateExtn,
-            getOnly: false,
-            setAndProcessOnly: false,
-            processOnly: false,
-            template: '',
             success: '',
             error: ''
         }, options);
+
+        var templateModel = model || {};
 
         var getTemplate = function (callback) {
             try {
@@ -26,8 +24,8 @@
                     callback($this[0][templateName]);
                 }
             } catch (e) {
-                var error = "An error occured while getting the template " + templateName + ". \n" + e;
-                onError(error);
+                var errorMessage = "An error occured while getting the template " + templateName + ". \n" + e.name + "\n" + e.message;
+                throwCustomException(e, errorMessage);
             }
         };
 
@@ -35,17 +33,17 @@
             try {
                 $this.setTemplate(template);
             } catch (e) {
-                var error = "An error occured while setting the template " + templateName + ". \n" + e;
-                onError(error);
+                var errorMessage = "An error occured while setting the template " + templateName + ". \n" + e.name + "\n" + e.message;
+                throwCustomException(e, errorMessage);
             }
         };
 
         var processTemplate = function () {
             try {
-                $this.processTemplate(model);
+                $this.processTemplate(templateModel);
             } catch (e) {
-                var error = "An error occured while processing the template " + templateName + ". \n" + e;
-                onError(error);
+                var errorMessage = "An error occured while processing the template " + templateName + ". \n" + e.name + "\n" + e.message;
+                throwCustomException(e, errorMessage);
             }
         };
 
@@ -61,28 +59,24 @@
             }
         };
 
+        var throwCustomException = function (innerException, customMessage) {
+            throw {
+                name: "Templatize Exception",
+                message: customMessage || "An error occurred in the templatization process. Refer the inner exception for details",
+                innerException: innerException
+            }
+        };
+
         return this.each(function () {
             $this = $(this);
-            if (settings.setAndProcessOnly) {
-                if (settings.template) {
-                    if (!settings.processOnly) {
-                        setTemplate(template);
-                    }
-                    processTemplate(model);
-                }
-            }
-            else {
-                getTemplate(function (data) {
-                    if (settings.getOnly) {
-                        onSuccess(data);
-                    }
-                    else {
-                        setTemplate(data);
-                        processTemplate(model);
-                        onSuccess();
-                    }
+            try {
+                $this.getTemplate(function (data) {
+                    $this.setTemplate(data);
+                    $this.processTemplate(templateModel);
                 });
+            } catch (e) {
+                onError(e)
             }
         });
-    };
+    };    
 })(jQuery);
